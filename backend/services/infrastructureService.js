16e8c8f5b6809bc23ff97a5edc452738d64a1c3a
Shachi Shukla <shachi.03shukla@gmail.com>
2026-07-22 21:58:40 +0530

    permit suspension flag service updated

--- backend/services/infrastructureService.js
+++ backend/services/infrastructureService.js
@@ -23,3 +23,48 @@ exports.update=async(id,data)=>{
 exports.delete=async(id)=>{
     return await Infrastructure.findByIdAndDelete(id);
 };
+
+
+exports.evaluateSuspensions = async (city, threshold = 250) => {
+
+    const res = await axios.get(
+        `http://localhost:8000/api/v1/forecast/${city}/wards`
+    );
+
+    const forecasts = res.data.forecastWards;
+
+    const highRiskWards = forecasts
+        .filter(w => w.aqi >= threshold)
+        .map(w => String(w.ward_no));
+
+    //  suspended permits -> active permits in safe wards
+    await Infrastructure.updateMany(
+        {
+            ward: { $nin: highRiskWards },
+            status: "Suspended (Recommended)"
+        },
+        {
+            $set: {
+                status: "Active"
+            }
+        }
+    );
+
+     // active permits -> suspended permits in high-risk wards
+    await Infrastructure.updateMany(
+        {
+            ward: { $in: highRiskWards },
+            status: "Active"
+        },
+        {
+            $set: {
+                status: "Suspended (Recommended)"
+
+            }
+        }
+    );
+
+    return await Infrastructure.find({
+        status: "Suspended (Recommended)"
+    });
+};
